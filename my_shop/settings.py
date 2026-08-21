c28b2af89c121e1d50f9af2a15ad36e137602a13
amuzyrev <amuzyrev@ozon.ru>
2026-08-21 13:51:21 +0300

    астройка S3 хранилища для медиа

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'storages',
 
     # Наши приложения
     'apps.accounts',
@@ -268,3 +269,38 @@ LOGGING = {
         },
     },
 }
+
+# ============ S3 ХРАНИЛИЩЕ ============
+# Используем S3 для хранения медиа
+USE_S3 = os.getenv('USE_S3', 'False') == 'True'
+
+if USE_S3:
+    # Настройки для Timeweb S3
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', 'maidlingerie-media')
+
+    # ВАЖНО! Используйте правильный эндпоинт из панели Timeweb
+    AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL', 'https://s3.twcstorage.ru')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'ru-1')
+
+    # Настройки для публичного доступа
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+
+    # Хранилище для медиа и статики
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+    # URL для доступа к файлам
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.twcstorage.ru/'
+    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.twcstorage.ru/static/'
+else:
+    # Локальное хранилище
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
