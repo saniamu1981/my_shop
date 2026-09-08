0e703b2526dd17f5adc2dd80fd81b0f1fd1fb208
amuzyrev <amuzyrev@ozon.ru>
2026-09-08 16:01:11 +0300

    Убрано поле local_path из админки

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -4,35 +4,10 @@ from django.utils.html import format_html
 from .models import Category, Product, ProductImage, ProductVideo, Favorite, ProductSize
 
 
-class ProductImageForm(forms.ModelForm):
-    local_path = forms.CharField(
-        label='Локальный путь (относительно media/)',
-        required=False,
-        help_text='Пример: products/kostyum_gornichnoj_rozovyj/1.jpg',
-        widget=forms.TextInput(attrs={'size': 80, 'style': 'width: 100%;'})
-    )
-
-    class Meta:
-        model = ProductImage
-        fields = ('product', 'image', 'is_main', 'order')
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        local_path = self.cleaned_data.get('local_path')
-        if local_path and instance.image:
-            # Сохраняем по локальному пути
-            instance.image.name = local_path
-            instance._local_path = local_path
-        if commit:
-            instance.save()
-        return instance
-
-
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
-    form = ProductImageForm
     extra = 3
-    fields = ('image', 'is_main', 'order', 'local_path', 'preview')
+    fields = ('image', 'is_main', 'order', 'preview')
     readonly_fields = ('preview',)
     ordering = ('order',)
 
@@ -85,34 +60,8 @@ class ProductSizeInline(admin.TabularInline):
     ordering = ('size',)
 
 
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Добавляем поле для локального пути к главному изображению
-        self.fields['local_path'] = forms.CharField(
-            label='Локальный путь главного фото (относительно media/)',
-            required=False,
-            help_text='Пример: products/kostyum_gornichnoj_rozovyj/1.jpg',
-            widget=forms.TextInput(attrs={'size': 80, 'style': 'width: 100%;'})
-        )
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        local_path = self.cleaned_data.get('local_path')
-        if local_path and instance.image:
-            instance.image.name = local_path
-        if commit:
-            instance.save()
-        return instance
-
-
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    form = ProductForm
     list_display = ('name', 'category', 'price', 'available', 'sku', 'created')
     list_filter = ('available', 'category', 'created')
     list_editable = ('price', 'available')
@@ -129,7 +78,7 @@ class ProductAdmin(admin.ModelAdmin):
             'fields': ('price', 'available')
         }),
         ('Главное изображение', {
-            'fields': ('image', 'local_path')
+            'fields': ('image',)
         }),
         ('Характеристики товара', {
             'fields': (
@@ -156,11 +105,9 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
-    form = ProductImageForm
     list_display = ('product', 'image_preview', 'is_main', 'order')
     list_filter = ('is_main', 'product')
     list_editable = ('is_main', 'order')
-    fields = ('product', 'image', 'local_path', 'is_main', 'order')
 
     def image_preview(self, obj):
         if obj.image:
